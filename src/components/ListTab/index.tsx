bdc314ea64987a391625eb2fc77d686d782f897e
marvelopers <marvelopers@gmail.com>
2021-08-21 13:45:03 +0900

    탭 메뉴 추가

---
+++ src/components/ListTab/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { TabBar } from '../common/TabBar';
+import MarketList from '../MarketList';
+import MarketListItem from '../MarketListItem';
+
+enum Menu {
+  MarKet = 'market',
+  Like = 'like',
+}
+
+const List = {
+  [Menu.MarKet]: '가상자산 시세목록',
+  [Menu.Like]: '북마크 목록',
+};
+
+const ListTab = () => {
+  console.log(List);
+  return (
+    <TabBar initIndex={0} tabList={['가상자산 시세목록', '북마크 목록']}>
+      <MarketList />
+      <MarketListItem />
+    </TabBar>
+  );
+};
+
+export default ListTab;
