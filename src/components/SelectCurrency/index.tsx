dd69b11ce159d29a31ceb20c0a595b2b5180b607
marvelopers <marvelopers@gmail.com>
2021-08-21 23:11:58 +0900

    통화 선택하기 기능 작업 완료

--- src/components/SelectCurrency/index.tsx
+++ src/components/SelectCurrency/index.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { ChangeEvent, useCallback } from 'react';
+import { useDispatch } from 'react-redux';
 import { CurrencyType, CurrencyTypeString } from 'src/constants/currency';
+import { MarketPriceActions } from 'src/features/market/marketSlice';
 import * as Styles from './styles';
 
-const SelectCurrency = () => (
-  <>
-    <Styles.List>
-      {Object.keys(CurrencyTypeString).map((key) => (
-        <Styles.Item key={key} value={key}>
-          {CurrencyTypeString[key as CurrencyType]}
-        </Styles.Item>
-      ))}
-    </Styles.List>
-  </>
-);
+const SelectCurrency = () => {
+  const dispatch = useDispatch();
+  const handleChangeCurrencyType = useCallback(
+    (e: ChangeEvent<HTMLSelectElement>) => {
+      dispatch(MarketPriceActions.changeCurrency(e.target.value as CurrencyType));
+    },
+    [dispatch],
+  );
+
+  return (
+    <Styles.Wrapper>
+      <Styles.List onChange={handleChangeCurrencyType}>
+        {Object.keys(CurrencyTypeString).map((key) => (
+          <Styles.Item key={key} value={key}>
+            {CurrencyTypeString[key as CurrencyType]}
+          </Styles.Item>
+        ))}
+      </Styles.List>
+    </Styles.Wrapper>
+  );
+};
 export default SelectCurrency;
--- src/components/SelectCurrency/styles.ts
+++ src/components/SelectCurrency/styles.ts
@@ -1,12 +1,30 @@
 import styled from '@emotion/styled';
+import { GREY_2 } from 'src/styles/colors';
+
+export const Wrapper = styled.div`
+  text-align: right;
+  padding-right: 1.5rem;
+  color: ${GREY_2};
+  border: none;
+`;
 
 export const List = styled.select`
   width: 100px;
   height: 40px;
   border: none;
   background-color: transparent;
+  color: ${GREY_2};
   font-size: 0.875rem;
   line-height: 1.25rem;
+  cursor: pointer;
+
+  -moz-appearance: none; /* Firefox */
+  -webkit-appearance: none; /* Safari and Chrome */
+  appearance: none;
+
+  &:focus {
+    outline: none;
+  }
 `;
 
 export const Item = styled.option`
