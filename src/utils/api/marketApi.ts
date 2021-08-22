244c015c9c7978a51f77e31caee4f5115e01a620
marvelopers <marvelopers@gmail.com>
2021-08-22 13:49:49 +0900

    api 데이터 확인 콘솔 삭제

--- src/utils/api/marketApi.ts
+++ src/utils/api/marketApi.ts
@@ -5,10 +5,7 @@ import { BASE_URL, HTTP } from './http';
 export const marketApi = {
   getMarket: async (params: Params): Promise<any> => {
     const queryString = qs.stringify(params);
-    console.log('data', params);
-
     const { data } = await HTTP.get(`${BASE_URL}/coins/markets?${queryString}`);
-    console.log('Market', data);
 
     return data;
   },
@@ -16,7 +13,6 @@ export const marketApi = {
     const { data } = await HTTP.get(
       `${BASE_URL}/coins/${id}?tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false`,
     );
-    console.log('data', data);
     return data;
   },
 };
