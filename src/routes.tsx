6c9afdddc8daa9f4d739614f4682cedd9b8ffe75
marvelopers <marvelopers@gmail.com>
2021-08-22 12:54:30 +0900

    목록별 페이지 루트 분리

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LikePage from './components/LikePage';
 import MarketPage from './components/MarketPage';
 
 const RoutesMenus = [
   {
     key: 'market',
     name: '오늘의 시세',
-    path: '/',
-    url: () => '/',
+    path: '/market',
+    url: () => '/market',
     component: MarketPage,
+  },
+  {
+    key: 'like',
+    name: '오늘의 시세',
+    path: '/like',
+    url: () => '/like',
+    component: LikePage,
   },
 ];
 
