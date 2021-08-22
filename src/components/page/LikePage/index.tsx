8f013e025361e3554da80dbdd4e7223ec75e92c0
marvelopers <marvelopers@gmail.com>
2021-08-22 14:26:50 +0900

    페이지 컴포넌트 페이지 폴더 하위로 구조 변경

--- src/components/page/LikePage/index.tsx
+++ src/components/page/LikePage/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { selectLikeCoin } from 'src/selectors/market';
 import { useSelector } from 'react-redux';
-import Layout from '../Layout';
-import MarketList from '../MarketList';
+import Layout from '../../Layout';
+import MarketList from '../../MarketList';
 
 const LikePage = () => {
   const LikeCoin = useSelector(selectLikeCoin);
@@ -19,7 +19,6 @@ const LikePage = () => {
 export default LikePage;
 
 const Wrapper = styled.div`
-  border: 1px solid pink;
   max-width: 1280px;
   margin: auto;
 `;
--- src/components/page/MarketPage/index.tsx
+++ src/components/page/MarketPage/index.tsx
@@ -3,9 +3,9 @@ import styled from '@emotion/styled';
 import { getMarketPrice } from 'src/features/market/marketSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectMarket } from 'src/selectors';
-import Layout from '../Layout';
-import MarketList from '../MarketList';
-import Loader from '../common/Loader';
+import Layout from '../../Layout';
+import MarketList from '../../MarketList';
+import Loader from '../../common/Loader';
 
 const MarketPage = () => {
   const dispatch = useDispatch();
@@ -27,7 +27,6 @@ const MarketPage = () => {
 export default MarketPage;
 
 const Wrapper = styled.div`
-  border: 1px solid pink;
   max-width: 1280px;
   margin: auto;
 `;
